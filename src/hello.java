9217a455b04974d9ac8546054dcdabb9bee49802
nehalanghe <nehalanghe@gmail.com>
2019-03-30 15:43:35 +0530

    changes through eclipse

--- src/hello.java
+++ src/hello.java
@@ -6,7 +6,7 @@ public class hello {
 		System.out.println("my first commit");
 		System.out.println("Dev2 updation");
 		System.out.println("changes through eclipse");
-System.out.println(changes by eclipse);
+        System.out.println("changes by eclipse");
 	}
 
 }
